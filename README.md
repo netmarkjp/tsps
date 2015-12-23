ebfb0a8075386ea0f59141a37bce58f39149c1e8
Toshiaki Baba <toshiaki@netmark.jp>
2015-12-24 01:18:29 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,31 @@
+# TSPS
+
+Time Series Photo Stock
+
+1. find jpeg image from ``--from`` directory
+2. copy jpeg image to ``--to`` directory
+    - ``--to`` / ``YYYYmm`` / ``YYYYmmdd`` / ``YYYYmmdd_HHMMSS-MD5SUM.jpg``
+
+Copy jpeg files from ``--from`` dir to ``--to`` dir.
+At the time of copying, filename sets to ``YYYYmmdd_HHMMSS-MD5SUM.jpg`` and
+saved to ``YYYYmm`` / ``YYYYmmdd`` .
+
+ex: ``201506/20150623/20150623_130623-e3b0c44298fc1c14e3b0c44298fc1c14.jpg``
+
+The time data get from EXIF date.
+When EXIF's datetime cannot get, use original file timestamp.
+
+# Usage
+
+```
+tsps --from /Volumes/NONAME --to ~/Pictures/mylife
+```
+
+May run with python 2.7
+
+# Install
+
+```
+pip install -r requirements.txt
+```
+
---
+++ tsps.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from logging import DEBUG
+from logging import INFO
+from logging import StreamHandler
+from logging import getLogger
+from multiprocessing import Pool
+import click
+import datetime
+import dateutil.parser
+import hashlib
+import os
+import piexif
+import re
+import shutil
+
+logger = getLogger(__name__)
+
+
+def find_jpeg_files(root_dir):
+    jpeg_ext_pattern = re.compile(".*\.jpe?g$", flags=re.IGNORECASE)
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            if jpeg_ext_pattern.match(file):
+                yield os.path.join(root, file)
+
+
+def process_store(jpeg_file, to_dir):
+    try:
+        exif_dict = piexif.load(jpeg_file)
+        exif_datetime_original = exif_dict["Exif"][
+            piexif.ExifIFD.DateTimeOriginal]
+        try:
+            datetime_original = datetime.datetime.strptime(
+                exif_datetime_original, "%Y:%m:%d %H:%M:%S")
+        except:
+            datetime_original = dateutil.parser.parse(exif_datetime_original)
+
+        YYmm, YYmmdd, YYmmdd_HHMMSS = get_datetime_str(datetime_original)
+
+        md5sum = md5(jpeg_file)
+
+        new_filename = "%s-%s.jpg" % (YYmmdd_HHMMSS, md5sum)
+
+        new_filepath = os.path.join(to_dir, YYmm, YYmmdd, new_filename)
+        if os.path.exists(new_filepath):
+            logger.info("%s already exist. orig=%s", new_filepath, jpeg_file)
+            return
+
+        logger.info("%s -> %s", jpeg_file, new_filepath)
+        new_dirpath = os.path.join(to_dir, YYmm, YYmmdd)
+        if not os.path.exists(new_dirpath):
+            os.makedirs(new_dirpath, mode=0755)
+        shutil.copy(jpeg_file, new_filepath)
+        raise Exception("hogeee")
+    except Exception as e:
+        logger.error("%s jpeg_file=%s", e, jpeg_file)
+
+
+def get_datetime_str(datetime_original):
+    YYmm = datetime.datetime.strftime(datetime_original, "%Y%m")
+    YYmmdd = datetime.datetime.strftime(datetime_original, "%Y%m%d")
+    YYmmdd_HHMMSS = datetime.datetime.strftime(
+        datetime_original, "%Y%m%d_%H%M%S")
+    return (YYmm, YYmmdd, YYmmdd_HHMMSS)
+
+
+def md5(filepath):
+    with open(filepath, "rb") as f:
+        data = f.read()
+    return hashlib.md5(data).hexdigest()
+
+
+def set_debug():
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+    handler = StreamHandler()
+    handler.setLevel(DEBUG)
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler)
+
+
+@click.command()
+@click.option("-f", "--from_dir", required=True)
+@click.option("-t", "--to_dir", required=True)
+@click.option("-d", "--debug", is_flag=True, default=False)
+def tsps(from_dir, to_dir, debug):
+    if debug:
+        set_debug()
+    pool = Pool(processes=8)
+    for jpeg_file in find_jpeg_files(from_dir):
+        logger.debug("=> %s", jpeg_file)
+        pool.apply_async(process_store, (jpeg_file, to_dir))
+    pool.close()
+    pool.join()
+
+if __name__ == "__main__":
+    handler = StreamHandler()
+    handler.setLevel(INFO)
+    logger.setLevel(INFO)
+    logger.addHandler(handler)
+    tsps()
